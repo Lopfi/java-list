7413395690da448dc2bd7c21a7a77cfef74a60ce
RobinD95 <lopfi95@gmail.com>
2021-01-26 15:40:12 +0100

    add BAUMDRUCKER class to print out BINBAUM's in a nice way

---
+++ src/com/go/baum/BAUMDRUCKER.java
@@ -0,0 +1,58 @@
+package com.go.baum;
+
+public class BAUMDRUCKER {
+
+        private BINBAUM baum;
+
+        public BAUMDRUCKER(BINBAUM baum) {
+            this.baum = baum;
+        }
+
+        private String traversePreOrder(BINBAUM binbaum) {
+
+            BAUMELEMENT root = binbaum.getWurzel();
+
+            if (root.datenGeben() == null) return "Der übergebene Baum ist leer";
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(root.datenGeben().getKey());
+
+            String pointerRight = "└──";
+            String pointerLeft = (root.nachfolgerRGeben() != null) ? "├──" : "└──";
+
+            traverseNodes(sb, "", pointerLeft, root.nachfolgerLGeben(), root.nachfolgerRGeben() != null);
+            traverseNodes(sb, "", pointerRight, root.nachfolgerRGeben(), false);
+
+            return sb.toString();
+        }
+
+        private void traverseNodes(StringBuilder sb, String padding, String pointer, BAUMELEMENT node, boolean hasRightSibling) {
+
+            if (node != null && !(node instanceof ASCHLUSS)) {
+
+                sb.append("\n");
+                sb.append(padding);
+                sb.append(pointer);
+                sb.append(node.datenGeben().getKey());
+
+                StringBuilder paddingBuilder = new StringBuilder(padding);
+                if (hasRightSibling) {
+                    paddingBuilder.append("│  ");
+                } else {
+                    paddingBuilder.append("   ");
+                }
+
+                String paddingForBoth = paddingBuilder.toString();
+                String pointerRight = "└──";
+                String pointerLeft = (node.nachfolgerRGeben() != null) ? "├──" : "└──";
+
+                traverseNodes(sb, paddingForBoth, pointerLeft, node.nachfolgerLGeben(), node.nachfolgerRGeben() != null);
+                traverseNodes(sb, paddingForBoth, pointerRight, node.nachfolgerRGeben(), false);
+
+            }
+        }
+
+        public void print(BINBAUM baum) {
+            System.out.println(this.traversePreOrder(baum));
+        }
+}
